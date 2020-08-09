ae67bcd6929e4abe9d01d4725a20a686f04f01db
hax0rtahm1d <tahmidrayat@gmail.com>
2020-08-09 16:06:40 +0600

    Bhai4You/Crazy-Banner

---
+++ Crazy-Banner/README.md
@@ -0,0 +1,42 @@
+<p align="center">
+<a href="#"><img title="succeed" src="https://img.shields.io/badge/deobfuscating-succeed-green?colorB=%23017e40&style=for-the-badge"></a>
+</p>
+<p align="left">
+<a href="https://github.com/hax0rtahm1d"><img title="HaX0r Tahm1d" src="https://img.shields.io/badge/By-HaX0r%20Tahm1d-blue?style=for-the-badge&logo=github"></a>
+</p>
+<br/><br/>
+
+# Crazy Banner
+#Script by Sutariya Parixit
+
+Installation Step :
+
+1) git clone https://github.com/Bhai4You/Crazy-Banner
+
+
+2) cd Crazy-Banner
+
+
+3) chmod +x requirement.sh
+
+
+4) chmod +x c-banner.sh
+
+
+5) bash requirement.sh
+
+
+6) bash c-banner.sh
+
+
+7) goto /sdcard/jkr/c-ban.sh and add Your ASCII art than save without any modification 😅
+
+
+8) than Continue Processing..
+
+
+9) Enter Your Banner Name
+
+
+10) Done..!!!
+
---
+++ Crazy-Banner/c-banner_dec.sh
@@ -0,0 +1,73 @@
+clear
+figlet -f big Custom Banner | lolcat
+cyan='\e[0;36m'
+lightgreen='\e[1;32m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+echo -e $lightgreen "\e[1m                      3vryth1n9   15   P0551613 !!!"
+echo " "
+echo -e $yellow "\e[1m                         -  Sutariya Parixit "
+echo " "
+echo " "
+sleep 3
+echo " "
+cd /sdcard
+mkdir jkr
+cd jkr
+touch c-ban.sh
+echo
+echo " " >> c-ban.sh
+echo "clear" >> c-ban.sh
+echo "cat << 'EOF' " >> c-ban.sh
+echo "  " >> c-ban.sh
+echo
+clear
+echo
+echo
+echo
+echo -e "\e[1m\e[33m\nðŸ˜ˆ Goto \e[36m/sdcard/jkr/c-ban.sh\e[33m and edit with any \e[36mtext Editor\e[33m to Add Your  \e[31mCustom Asciii \e[33m Banner\e[32m :\n\n"
+echo
+read bull
+echo
+clear
+echo
+echo
+echo
+echo -e " \e[0m\e[1m           Save \e[36m\e[1mc-ban.sh \e[0m\e[1mand Back to \e[36m\e[1mTermux app...\e[0m "
+echo
+read anonymous
+echo
+echo
+sleep 5
+echo
+clear
+echo
+echo
+echo
+echo -e " \e[1m\e[33m\e[1mNote :\e[0m \e[1m Please Edit Your C-ban file in Sdcard than Continue This Process...!!!"
+echo
+read parixit
+echo
+echo
+clear
+echo
+echo
+echo
+echo -e "\e[1m\e[32m\e[1m          3\e[0m \e[1mTime Press Enter To Continue...       "
+read bhai
+read for
+read you
+echo "  " >> c-ban.sh
+echo "EOF" >> c-ban.sh
+echo
+cat "c-ban.sh" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo " "
+echo " "
+echo " "
+echo -e "\e[1m\e[33m\nWhat is Your \e[31mBanner \e[33mName\e[32m :\n\n"
+read varbanner
+echo
+echo
+echo "toilet -f big ' $varbanner' -F gay | lolcat" > 84nn3r.txt
+echo
+cat "84nn3r.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
