a8bbdf9887282c6ef76aef1104bbe1704263c380
hax0rtahm1d <tahmidrayat@gmail.com>
2020-08-11 16:55:44 +0600

    RANDIOLOY/decompile

---
+++ decompile/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+clear
+pkg install figlet
+clear
+figlet install | lolcat
+sleep 2
+clear
+pkg install nano
+clear
+pkg install python
+clear
+pkg install python2
+clear
+pkg install libcurl
+clear
+gem install lolcat
+clear
+pip2 install uncompyle6
+clear
+pip2 install mechanize
+clear
+pip2 install termocolor
+echo "selesai"
+clear
+python2 decom.pyc
