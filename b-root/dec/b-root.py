547fbbe4c0da4eed29a27e76545139ddbf71ac86
hax0rtahm1d <tahmidrayat@gmail.com>
2020-08-30 11:59:26 +0000

    Binyamin-binni/b-root

---
+++ b-root/dec/b-root.py
@@ -0,0 +1,9 @@
+# Deobfuscated BY HTR-TECH | Tahmid Rayat
+
+# Github    : https://github.com/htr-tech 
+# Instagram : https://www.instagram.com/tahmid.rayat
+# Facebook  : https://fb.com/tahmid.rayat.oficial 
+# Messenger : https://m.me/tahmid.rayat.oficial 
+
+import os,sys
+os.system("cd /data/data/com.termux/files/home/ && pkg install wget openssl-tool proot -y && hash -r && bash /data/data/com.termux/files/home/b-root/.README.md")
