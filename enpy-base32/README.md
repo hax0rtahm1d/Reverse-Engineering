d61fcb13e70b9b9181681d2d1f389d31a07df5d7
hax0rtahm1d <tahmidrayat@gmail.com>
2020-08-30 12:17:31 +0000

    hatakecnk/enpy-base32

---
+++ enpy-base32/README.md
@@ -0,0 +1,28 @@
+<p align="center">
+<a href="#"><img title="succeed" src="https://img.shields.io/badge/deobfuscating-succeed-green?colorB=%23017e40&style=for-the-badge"></a>
+</p>
+<p align="left">
+<a href="https://github.com/hax0rtahm1d"><img title="HaX0r Tahm1d" src="https://img.shields.io/badge/By-HaX0r%20Tahm1d-blue?style=for-the-badge&logo=github"></a>
+</p>
+<br/><br/>
+
+# enpy-base32
+Encrypt Your Python Code With Base32
+
+![alt text](https://img.shields.io/badge/Coded-xNot_Found-blue.svg)
+![alt text](https://img.shields.io/badge/Size-33.00KB-yellow.svg)
+![alt text](https://img.shields.io/badge/Python-2.7-green.svg)
+# Sample Image
+![alt text](https://raw.githubusercontent.com/hatakecnk/hatakecnk.github.io/master/IMG_20190602_185435.jpg)
+
+# How To Install
+```
+$ pkg install git python2
+$ git clone https://github.com/hatakecnk/enpy-base32
+```
+
+# How To Execute
+```
+$ python2 enpy-base32/enpy-base32.py
+```
+
---
+++ enpy-base32/enpy-base32_dec.py
@@ -0,0 +1,68 @@
+# Deobfuscated BY HTR-TECH | Tahmid Rayat
+
+# Github    : https://github.com/htr-tech 
+# Instagram : https://www.instagram.com/tahmid.rayat
+# Facebook  : https://fb.com/tahmid.rayat.oficial 
+# Messenger : https://m.me/tahmid.rayat.oficial 
+
+import base64, sys, os, time
+B = '\x1b[34m'
+R = '\x1b[31m'
+G = '\x1b[32m'
+W = '\x1b[0m'
+Y = '\x1b[33;5m'
+
+def banner():
+    os.system('clear')
+    print R + '                        _                  _______'
+    print R + ' ___ _ _  _ __ _  _ ___| |__  __ _ ___ ___|__ /_  )'
+    print R + "/ -_) ' \\| '_ \\ || |___| '_ \\/ _` (_-</ -_)|_ \\/ /"
+    print W + '\\___|_||_| .__/\\_, |   |_.__/\\__,_/__/\\___|___/___|'
+    print W + '         |_|   |__/'
+    print Y + '0{' + 47 * '=' + '}0'
+    print Y + '|' + B + ' Coded by: ' + W + 'xNot_Found' + Y + '                            |'
+    print Y + '|' + B + ' Contact : ' + W + '+62823-8637-2115' + Y + '                      |'
+    print Y + '|' + B + ' Email   : ' + W + 'febryafriansyah@programmer.net' + Y + '        |'
+    print Y + '|' + B + ' Website : ' + W + 'http://hatakecnk.noads.biz' + Y + '            |'
+    print Y + '|' + B + ' Github  : ' + W + 'https://github.com/hatakecnk' + Y + '          |'
+    print Y + '0{' + 47 * '=' + '}0\n'
+
+
+try:
+    banner()
+    file = raw_input('\x1b[0m[\x1b[31m Input Your File /path/file.py \x1b[0m]> \x1b[36m')
+except IndexError:
+    print R + '[' + W + '!' + R + '] ' + W + 'there is an error\n'
+    sys.exit()
+except KeyboardInterrupt:
+    print R + '[' + W + '!' + R + '] ' + W + 'ctrl+c detected'
+    print R + '[' + W + '!' + R + '] ' + W + 'trying to exit\n'
+    time.sleep(3)
+    sys.exit()
+except EOFError:
+    print R + '\n[' + W + '!' + R + '] ' + W + 'ctrl+d detected'
+    print R + '[' + W + '!' + R + '] ' + W + 'trying to exit\n'
+    time.sleep(3)
+    sys.exit()
+else:
+    try:
+        fileopen = open(file).read()
+    except IOError:
+        banner()
+        print R + '[' + W + '!' + R + '] ' + W + 'file not exist\n'
+        sys.exit()
+
+    try:
+        a = base64.b32encode(fileopen)
+    except TypeError:
+        banner()
+        print R + '[' + W + '!' + R + '] ' + W + 'file already encrypted\n'
+        sys.exit()
+
+b = "#Encrypted By xNot_Found\n#Github : https://github.com/hatakecnk/\nimport base64\nexec(base64.b32decode('" + a + "'))"
+c = file.replace('.py', '_enc.py')
+d = open(c, 'w')
+d.write(b)
+d.close()
+banner()
+print B + '[' + W + '+' + B + '] ' + G + 'file saved : ' + W + c
